aa1b80a51bbea340a9079d1c254ba1de6765b7d7
Timothy W. Hilton <thilton@ucmerced.edu>
2014-04-07 13:19:06 -0700

    corrected description in setup.py (I forgot to replace the template string

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from distutils.core import setup
 setup(name='idwnn_kdtree_interp',
       version='1.0',
       py_modules=['idwnn_kdtree_interp'],
-      description='Python Distribution Utilities',
+      description='inverse distance weighted nearest neighbor
+        interpolation using a K-dimensional tree',
       author='Timothy W. Hilton',
       author_email='thilton@ucmerced.edu',
       )
