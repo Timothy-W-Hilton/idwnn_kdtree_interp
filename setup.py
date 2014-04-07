f99d277ced883ca071cad266484a5b6b91a7db34
Timothy W. Hilton <thilton@ucmerced.edu>
2014-04-07 11:50:26 -0700

    made idwnn_kdtree_interp module into a distributable python package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='idwnn_kdtree_interp',
+      version='1.0',
+      py_modules=['idwnn_kdtree_interp'],
+      description='Python Distribution Utilities',
+      author='Timothy W. Hilton',
+      author_email='thilton@ucmerced.edu',
+      )
