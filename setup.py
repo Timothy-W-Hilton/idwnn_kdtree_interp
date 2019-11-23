97347de5f85a29e7552bf17838b9dd401d44fec4
Timothy W. Hilton <twhilton@ucsc.edu>
2019-11-23 15:46:30 -0700

    updated email address, whitespace in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(name='idwnn_kdtree_interp',
       version='1.0',
       py_modules=['idwnn_kdtree_interp'],
-      description='inverse distance weighted nearest neighbor
-        interpolation using a K-dimensional tree',
+      description=('inverse distance weighted nearest neighbor'
+                   'interpolation using a K-dimensional tree'),
       author='Timothy W. Hilton',
-      author_email='thilton@ucmerced.edu',
+      author_email='thilton@ucsantacruz.edu',
       )
